2cce5b2d372c6f3604290343f83e460b396b407c
Jackob2004 <zjakub2004@gmail.com>
2026-03-08 17:43:13 +0100

    feat(recruiting state): add lobby/map switching
    Let player switch and quickly discover key map locations.
    
    Closes #13

--- src/main/kotlin/com/jackob/dvz/core/states/RecruitingState.kt
+++ src/main/kotlin/com/jackob/dvz/core/states/RecruitingState.kt
@@ -3,6 +3,7 @@ package com.jackob.dvz.core.states
 import com.jackob.dvz.storage.ConfigStorage
 import com.jackob.dvz.storage.GameMap
 import com.jackob.dvz.storage.MapStorage
+import com.jackob.dvz.ui.Menu
 import com.jackob.dvz.util.createItem
 import com.jackob.dvz.util.description
 import com.jackob.dvz.util.mm
@@ -21,6 +22,7 @@ import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.inventory.InventoryHolder
 
 private const val INFO_BAR_MAP = "<gray><b>Map: <reset><gradient:#11998e:#38ef7d><i>"
 private const val INFO_BAR_PLAYERS = "<reset><dark_gray><b>| <gray><b>Players: <reset><gradient:#38ef7d:#11998e><i>"
@@ -37,6 +39,8 @@ class RecruitingState(var gameMap: GameMap) : GameState {
     var wasMapRerolled = false
 
     var playersWaiting = 0
+
+    var teleportOptionsMenu = recreateTeleportOptions()
 
     override fun onEnter() {
         super.onEnter()
@@ -58,8 +62,88 @@ class RecruitingState(var gameMap: GameMap) : GameState {
         player.inventory.addItem(teleportTool)
     }
 
-    private fun openTeleportOptions(player: Player) {
-        TODO("Implement this")
+    private fun recreateTeleportOptions() : InventoryHolder {
+        return Menu.create("<white><b>Teleport options") {
+            val secondRow = (1..gameMap.shrines.size).joinToString("").padEnd(9, '_')
+            pattern(
+                "LDZ______",
+                secondRow,
+                "GSO______"
+            )
+
+            for ((index, location) in gameMap.shrines.withIndex()) {
+                button(index.plus(1).digitToChar()) {
+                    icon = createItem(Material.ENCHANTING_TABLE) {
+                        name = "<yellow>Shrine (#${index + 1})"
+                        onClick = {
+                            it.teleportAsync(location)
+                            it.closeInventory()
+                        }
+                    }
+                }
+            }
+
+            button('L') {
+                icon = createItem(Material.DIRT) {
+                    name = "<white>Lobby"
+                    onClick = {
+                        it.teleport(MapStorage.LOBBY_SPAWN!!)
+                        it.closeInventory()
+                    }
+                }
+            }
+
+            button('D') {
+                icon = createItem(Material.PLAYER_HEAD) {
+                    name = "<dark_green>Dwarf spawn"
+                }
+                onClick = {
+                    it.teleportAsync(gameMap.dwarfSpawn)
+                    it.closeInventory()
+                }
+            }
+
+            button('Z') {
+                icon = createItem(Material.ZOMBIE_HEAD) {
+                    name = "<dark_red>Zombie spawn"
+                    onClick = {
+                        it.teleportAsync(gameMap.zombieSpawn)
+                        it.closeInventory()
+                    }
+                }
+            }
+
+            button('G') {
+                icon = createItem(Material.GOLD_BLOCK) {
+                    name = "<gold>Goldmine"
+                    onClick = {
+                        it.teleportAsync(gameMap.goldmine)
+                        it.closeInventory()
+                    }
+                }
+            }
+
+            button('S') {
+                icon = createItem(Material.IRON_BARS) {
+                    name = "<white>Sawmill"
+                    onClick = {
+                        it.teleportAsync(gameMap.sawmill)
+                        it.closeInventory()
+                    }
+                }
+            }
+
+            button('O') {
+                icon = createItem(Material.SPONGE) {
+                    name = "<dark_purple>Oil"
+                    onClick = {
+                        it.teleportAsync(gameMap.oil)
+                        it.closeInventory()
+                    }
+                }
+            }
+
+        }
     }
 
     private fun updateInfoBar() {
@@ -71,7 +155,7 @@ class RecruitingState(var gameMap: GameMap) : GameState {
      * Applies behavior/options associated to the recruiting state
      */
     private fun refreshPlayer(player: Player) {
-        player.teleport(MapStorage.LOBBY_SPAWN!!)
+        player.teleportAsync(MapStorage.LOBBY_SPAWN!!)
         player.resetAll()
         player.closeInventory()
         giveLobbyTools(player)
@@ -82,6 +166,7 @@ class RecruitingState(var gameMap: GameMap) : GameState {
         gameMap = newMap
 
         updateInfoBar()
+        teleportOptionsMenu = recreateTeleportOptions()
         Bukkit.broadcast("<gray><i>Map was Changed!!!".withPrefix().mm())
         Bukkit.getOnlinePlayers().forEach {
             refreshPlayer(it)
@@ -95,6 +180,7 @@ class RecruitingState(var gameMap: GameMap) : GameState {
         gameMap = rerolledMap
 
         updateInfoBar()
+        teleportOptionsMenu = recreateTeleportOptions()
         Bukkit.broadcast("<gray><i>Map Rerolled!!!".withPrefix().mm())
         Bukkit.getOnlinePlayers().forEach {
             refreshPlayer(it)
@@ -118,6 +204,13 @@ class RecruitingState(var gameMap: GameMap) : GameState {
 
     @EventHandler
     fun onLobbyToolClick(event: PlayerInteractEvent) {
+        val lobbyTool = event.item ?: return
+        if (!event.action.isRightClick) return
+
+        when (lobbyTool.type) {
+            Material.COMPASS -> event.player.openInventory(teleportOptionsMenu.inventory)
+            else -> Unit
+        }
     }
 
     @EventHandler(priority = EventPriority.HIGH)
