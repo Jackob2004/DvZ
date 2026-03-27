fdc27dbbf2f357b333defac78ed357df45e51c37
Jackob2004 <zjakub2004@gmail.com>
2026-03-27 15:20:11 +0100

    feat(kits): add KitsManager
    Keeps track of Kit instances assigned to players.
    
    Refs: #17

---
+++ src/main/kotlin/com/jackob/dvz/kits/KitsManager.kt
@@ -0,0 +1,26 @@
+package com.jackob.dvz.kits
+
+import org.bukkit.entity.Player
+
+object KitsManager {
+
+    private val playerKits: MutableMap<Player, Kit> = HashMap()
+
+    fun setKit(player: Player, type: KitType) {
+        if (playerKits.containsKey(player)) return
+
+        playerKits[player] =
+            type.kitClass.getConstructor(String::class.java, Player::class.java).newInstance(type.toString(), player)
+                .apply {
+                    onActivate()
+                }
+    }
+
+    fun unsetKit(player: Player) {
+        playerKits[player]?.onDeactivate()
+    }
+
+    fun hasKit(player: Player) = playerKits.containsKey(player)
+
+    fun getKit(player: Player) = playerKits[player]
+}
