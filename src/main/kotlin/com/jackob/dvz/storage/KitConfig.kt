0d1c1c5da14a74267822685d66878895c722a34d
Jackob2004 <zjakub2004@gmail.com>
2026-03-27 14:21:49 +0100

    feat(kits): add extension functions to KitConfig data classes
    Will help with converting data classes to bukkit objects.
    
    Refs: #17

--- src/main/kotlin/com/jackob/dvz/storage/KitConfig.kt
+++ src/main/kotlin/com/jackob/dvz/storage/KitConfig.kt
@@ -2,7 +2,20 @@ package com.jackob.dvz.storage
 
 import com.charleskorn.kaml.Yaml
 import com.jackob.dvz.DvZ
+import com.jackob.dvz.util.mm
+import com.jackob.dvz.util.toAttribute
+import io.papermc.paper.registry.RegistryAccess
+import io.papermc.paper.registry.RegistryKey
 import kotlinx.serialization.Serializable
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.Registry
+import org.bukkit.attribute.AttributeModifier
+import org.bukkit.inventory.EquipmentSlotGroup
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.PotionMeta
+import org.bukkit.potion.PotionEffect
 import java.io.File
 
 const val FILE_NAME = "kits_config.yml"
@@ -18,6 +31,7 @@ data class PotionConfig(
 data class AttributeConfig(
     val type: String,
     val amount: Double,
+    val operation: String = "ADD_NUMBER",
     val slot: String? = null
 )
 
@@ -45,6 +59,63 @@ data class KitConfig(
 data class KitConfigRegistry(
     val kits: Map<String, KitConfig>
 )
+
+fun PotionConfig.toPotionEffect(): PotionEffect {
+    val type = Registry.POTION_EFFECT_TYPE.get(NamespacedKey.minecraft(type.lowercase()))!!
+
+    return PotionEffect(type, durationSeconds * 20, amplifier)
+}
+
+fun AttributeConfig.toAttributeModifier(): AttributeModifier {
+    val operation = AttributeModifier.Operation.valueOf(this.operation)
+    val slotGroup = when (slot?.uppercase()) {
+        "HAND" -> EquipmentSlotGroup.HAND
+        "HEAD" -> EquipmentSlotGroup.HEAD
+        "CHEST" -> EquipmentSlotGroup.CHEST
+        "LEGS" -> EquipmentSlotGroup.LEGS
+        "FEET" -> EquipmentSlotGroup.FEET
+        "ARMOR" -> EquipmentSlotGroup.ARMOR
+        else -> EquipmentSlotGroup.ANY
+    }
+
+    return AttributeModifier(NamespacedKey(DvZ.INSTANCE, "custom$type"), amount, operation, slotGroup)
+}
+
+fun ItemConfig.toItemStack(): ItemStack {
+    val material = Material.matchMaterial(material)!!
+    val name = name.mm()
+    val description = lore.map(String::mm)
+
+    val item = ItemStack(material)
+    val meta = item.itemMeta
+    meta.displayName(name)
+    meta.lore(description)
+
+    enchantments.forEach { (key, level) ->
+        val enchant = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT)
+            .get(NamespacedKey.minecraft(key.lowercase()))!!
+        meta.addEnchant(enchant, level, true)
+    }
+
+    attributes.forEach { attrConfig ->
+        meta.addAttributeModifier(attrConfig.type.toAttribute(), attrConfig.toAttributeModifier())
+    }
+
+    if (meta is PotionMeta) {
+        meta.basePotionType = Registry.POTION.get(NamespacedKey.minecraft(basePotionType!!.lowercase()))
+
+        val colorInt = potionColor!!.removePrefix("#").toIntOrNull(16)
+        if (colorInt != null) meta.color = Color.fromRGB(colorInt)
+
+        potionEffects.forEach { pConfig ->
+            meta.addCustomEffect(pConfig.toPotionEffect(), true)
+        }
+    }
+
+    item.itemMeta = meta
+
+    return item
+}
 
 fun getKitConfigs(): KitConfigRegistry? {
     val file = File(DvZ.INSTANCE.dataFolder, FILE_NAME)
--- src/main/kotlin/com/jackob/dvz/util/StringExtensions.kt
+++ src/main/kotlin/com/jackob/dvz/util/StringExtensions.kt
@@ -2,6 +2,9 @@ package com.jackob.dvz.util
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.NamespacedKey
+import org.bukkit.Registry
+import org.bukkit.attribute.Attribute
 
 const val PREFIX = "<gold><b>DvZ: <reset>"
 
@@ -11,4 +14,8 @@ fun String.mm(): Component {
 
 fun String.withPrefix(): String {
     return PREFIX + this
+}
+
+fun String.toAttribute() : Attribute {
+    return Registry.ATTRIBUTE.get(NamespacedKey.minecraft(this.lowercase()))!!
 }
