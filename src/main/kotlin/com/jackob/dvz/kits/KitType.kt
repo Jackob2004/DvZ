e1b727d6dfbd6a1132541c077974eddda1040684
Jackob2004 <zjakub2004@gmail.com>
2026-03-27 09:59:35 +0100

    feat(kits): add id property in KitType
    Refs: #17

--- src/main/kotlin/com/jackob/dvz/kits/KitType.kt
+++ src/main/kotlin/com/jackob/dvz/kits/KitType.kt
@@ -1,6 +1,13 @@
 package com.jackob.dvz.kits
 
 import com.jackob.dvz.storage.KitDisplay
+import org.bukkit.NamespacedKey
 
-enum class KitType(val kitClass: Class<out Kit>, val team: Team, val isHero: Boolean, val displayData: KitDisplay) {
+enum class KitType(
+    val kitClass: Class<out Kit>,
+    val team: Team,
+    val isHero: Boolean,
+    val id: NamespacedKey,
+    val displayData: KitDisplay
+) {
 }
