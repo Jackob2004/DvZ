3b91240a0ba6cb5e053bda00254d75dd8dc6a713
Jackob2004 <zjakub2004@gmail.com>
2026-03-27 16:18:32 +0100

    feat(kits): define functions in Kit interface
    Refs: #17

--- src/main/kotlin/com/jackob/dvz/kits/Kit.kt
+++ src/main/kotlin/com/jackob/dvz/kits/Kit.kt
@@ -1,4 +1,34 @@
 package com.jackob.dvz.kits
 
+import org.bukkit.entity.Player
+
 interface Kit {
+
+    fun getName(): String
+
+    fun getPlayer(): Player
+
+    fun onActivate() {
+        applyPotionEffects()
+        applyAttributeModifiers()
+        giveItems()
+    }
+
+    fun onDeactivate() { }
+
+    fun applyPotionEffects() {
+        getPlayer().addPotionEffects(KitConfigsCache.retrieveKitPotions(getName()))
+    }
+
+    fun applyAttributeModifiers() {
+        KitConfigsCache.retrieveAttributes(getName()).forEach {
+            getPlayer().getAttribute(it.first)!!.addModifier(it.second)
+        }
+    }
+
+    fun giveItems() {
+        KitConfigsCache.retrieveKitItems(getName()).forEach {
+            getPlayer().inventory.addItem(it)
+        }
+    }
 }
