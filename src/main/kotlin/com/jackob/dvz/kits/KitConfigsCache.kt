137638cdea7b7127d55b0e06dd77d685aa176b60
Jackob2004 <zjakub2004@gmail.com>
2026-03-27 15:07:44 +0100

    feat(kits): add KitConfigsCache
    Will serve as cache for kit configs.
    Classes implementing Kit interface will leverage it.
    
    Refs: #17

---
+++ src/main/kotlin/com/jackob/dvz/kits/KitConfigsCache.kt
@@ -0,0 +1,28 @@
+package com.jackob.dvz.kits
+
+import com.jackob.dvz.storage.getKitConfigs
+import com.jackob.dvz.storage.toAttributeModifier
+import com.jackob.dvz.storage.toItemStack
+import com.jackob.dvz.storage.toPotionEffect
+import com.jackob.dvz.util.toAttribute
+import org.bukkit.attribute.Attribute
+import org.bukkit.attribute.AttributeModifier
+import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
+
+object KitConfigsCache {
+
+    private val kitsCache = getKitConfigs()!!.kits
+
+    fun retrieveKitPotions(kitName: String) : List<PotionEffect> {
+        return kitsCache[kitName]!!.potions.map { it.toPotionEffect() }
+    }
+
+    fun retrieveAttributes(kitName: String) : List<Pair<Attribute, AttributeModifier>> {
+        return kitsCache[kitName]!!.attributes.map { Pair(it.type.toAttribute(), it.toAttributeModifier()) }
+    }
+
+    fun retrieveKitItems(kitName: String) : List<ItemStack> {
+        return kitsCache[kitName]!!.items.map { it.toItemStack() }
+    }
+}
