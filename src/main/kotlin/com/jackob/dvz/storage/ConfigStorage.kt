b73f502cc18a86c50e1af928399a09d48e6601ca
Jackob2004 <zjakub2004@gmail.com>
2026-04-02 18:48:11 +0200

    feat(config): add hero related config values
    Refs: #21

--- src/main/kotlin/com/jackob/dvz/storage/ConfigStorage.kt
+++ src/main/kotlin/com/jackob/dvz/storage/ConfigStorage.kt
@@ -6,4 +6,6 @@ object ConfigStorage {
     val REQUIRED_PLAYERS = DvZ.INSTANCE.config.getInt("required-players")
     val COUNTDOWN = DvZ.INSTANCE.config.getInt("countdown")
     val MAP_CHANGE_TIME_LIMIT = DvZ.INSTANCE.config.getInt("map-change-time-limit")
+    val PLAYERS_PER_HERO = DvZ.INSTANCE.config.getInt("players-per-hero")
+    val HERO_SELECT_TIME = DvZ.INSTANCE.config.getInt("hero-select-time")
 }
