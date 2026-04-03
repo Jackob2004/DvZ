ac8ac1176bc1c1756e396355de4419b5003e59a0
Jackob2004 <zjakub2004@gmail.com>
2026-04-03 16:23:44 +0200

    feat(preparation-state): add player join event handling
    Delegate to LobbyHelper when there is a need.
    Handle situatios in which: player is a newcomer, player was last active
    in recruiting, player is active dwarf.
    
    Closes: #22

--- src/main/kotlin/com/jackob/dvz/core/states/PreparationState.kt
+++ src/main/kotlin/com/jackob/dvz/core/states/PreparationState.kt
@@ -1,15 +1,33 @@
 package com.jackob.dvz.core.states
 
+import com.jackob.dvz.core.handleLobbyDamage
+import com.jackob.dvz.core.handleLobbyInteract
+import com.jackob.dvz.core.handleLobbyInvClick
+import com.jackob.dvz.core.handleLobbyItemDrop
+import com.jackob.dvz.core.handleLobbyToolClick
+import com.jackob.dvz.core.refreshToLobbyState
 import com.jackob.dvz.kits.KitType
 import com.jackob.dvz.kits.KitsManager
 import com.jackob.dvz.kits.Team
 import com.jackob.dvz.storage.GameMap
+import com.jackob.dvz.ui.PagerMenu
+import com.jackob.dvz.util.createItem
 import com.jackob.dvz.util.mm
+import com.jackob.dvz.util.name
 import com.jackob.dvz.util.resetAll
 import com.jackob.dvz.util.withPrefix
 import org.bukkit.Bukkit
 import org.bukkit.Sound
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.scoreboard.Team as BukkitTeam
 
 class PreparationState(private val gameMap: GameMap, private val selectedKits: Map<Player, KitType>) : GameState {
@@ -22,13 +40,53 @@ class PreparationState(private val gameMap: GameMap, private val selectedKits: M
         color(Team.DWARF.color)
     }
 
+    private fun isActiveDwarf(player: Player): Boolean {
+        return dwarfTeam.hasPlayer(player) && KitsManager.hasKit(player)
+    }
+
+    private fun lastActiveInRecruiting(player: Player): Boolean {
+        return !dwarfTeam.hasPlayer(player) && selectedKits.containsKey(player)
+    }
+
+    private fun addDwarf(player: Player, kitType: KitType) {
+        player.resetAll()
+        player.scoreboard = customBoard
+        dwarfTeam.addPlayer(player)
+        player.teleport(gameMap.dwarfSpawn)
+        KitsManager.setKit(player, kitType)
+    }
+
+    private fun openKitSelectionMenu(player: Player) {
+        val basicDwarfKits = KitType.entries
+            .filter { !it.isHero && it.team == Team.DWARF }
+            .map {
+                createItem(it.displayData.icon) {
+                    name = it.displayData.name
+                    lore(it.displayData.description.map(String::mm))
+                    persistentDataContainer.set(it.key, PersistentDataType.BOOLEAN, false)
+                }
+            }
+
+        object : PagerMenu(basicDwarfKits, player, "<gray><b>Select kit") {
+            override fun handleClick(slot: Int, player: Player) {
+                super.handleClick(slot, player)
+
+                menu.getItem(slot)?.let { item ->
+                    val keys = item.persistentDataContainer.keys
+                    if (keys.isEmpty()) return@handleClick
+
+                    KitType.getByKey(keys.first())?.let { type ->
+                        addDwarf(player, type)
+                    }
+                }
+
+            }
+        }
+    }
+
     override fun onEnter() {
         selectedKits.filter { (player, _) -> player.isOnline }.forEach { (onlinePlayer, kitType) ->
-            onlinePlayer.resetAll()
-            onlinePlayer.scoreboard = customBoard
-            dwarfTeam.addPlayer(onlinePlayer)
-            onlinePlayer.teleport(gameMap.dwarfSpawn)
-            KitsManager.setKit(onlinePlayer, kitType)
+            addDwarf(onlinePlayer, kitType)
         }
 
         Bukkit.broadcast("<gray>Preparation phase has started, dwarfs prepare for battle!!!".withPrefix().mm())
@@ -36,4 +94,47 @@ class PreparationState(private val gameMap: GameMap, private val selectedKits: M
 
         super.onEnter()
     }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        player.scoreboard = customBoard
+
+        if (!isActiveDwarf(player)) {
+            refreshToLobbyState(player)
+        } else if (lastActiveInRecruiting(player)) {
+            addDwarf(player, selectedKits[player]!!)
+        }
+    }
+
+    @EventHandler
+    fun onToolClick(event: PlayerInteractEvent) {
+        handleLobbyToolClick(event, ::openKitSelectionMenu)
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onInteract(event: PlayerInteractEvent) {
+        handleLobbyInteract(event)
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onEqInteract(event: InventoryClickEvent) {
+        handleLobbyInvClick(event)
+    }
+
+    @EventHandler
+    fun onDamage(event: EntityDamageEvent) {
+        handleLobbyDamage(event)
+    }
+
+    @EventHandler
+    fun onItemDrop(event: PlayerDropItemEvent) {
+        handleLobbyItemDrop(event)
+    }
+
+    @EventHandler
+    fun onFoodLevelChange(event: FoodLevelChangeEvent) {
+        event.foodLevel = 20
+    }
+
 }
