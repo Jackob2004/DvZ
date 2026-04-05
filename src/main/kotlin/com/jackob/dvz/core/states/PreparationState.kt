be0e9d3d4cb32e551d1e231e753dbf09da9c79e6
Jackob2004 <zjakub2004@gmail.com>
2026-04-05 18:01:51 +0200

    feat(preparation-state): add async countdown and info sidebar
    Using concurrent collection I can safely send async packets.
    
    Closes: #23

--- src/main/kotlin/com/jackob/dvz/core/states/PreparationState.kt
+++ src/main/kotlin/com/jackob/dvz/core/states/PreparationState.kt
@@ -9,8 +9,12 @@ import com.jackob.dvz.core.refreshToLobbyState
 import com.jackob.dvz.kits.KitType
 import com.jackob.dvz.kits.KitsManager
 import com.jackob.dvz.kits.Team
+import com.jackob.dvz.storage.ConfigStorage
 import com.jackob.dvz.storage.GameMap
 import com.jackob.dvz.ui.PagerMenu
+import com.jackob.dvz.ui.Sidebar
+import com.jackob.dvz.util.TimeUnit
+import com.jackob.dvz.util.async
 import com.jackob.dvz.util.createItem
 import com.jackob.dvz.util.mm
 import com.jackob.dvz.util.name
@@ -27,7 +31,10 @@ import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.persistence.PersistentDataType
+import java.util.concurrent.CopyOnWriteArraySet
+import java.util.concurrent.atomic.AtomicInteger
 import org.bukkit.scoreboard.Team as BukkitTeam
 
 class PreparationState(private val gameMap: GameMap, private val selectedKits: Map<Player, KitType>) : GameState {
@@ -40,13 +47,48 @@ class PreparationState(private val gameMap: GameMap, private val selectedKits: M
         color(Team.DWARF.color)
     }
 
-    override fun onEnter() {
-        selectedKits.filter { (player, _) -> player.isOnline }.forEach { (onlinePlayer, kitType) ->
-            addDwarf(onlinePlayer, kitType)
+    private val onlinePlayers = CopyOnWriteArraySet<Player>()
+
+    private val onlineDwarfs = AtomicInteger()
+
+    private val gameStatusSidebar =
+        Sidebar.create("<shadow:#000000:0.5><b><gradient:#1b4332:#2d6a4f:#74c69d>DWARVES VS ZOMBIES</gradient></b>") {
+            line(4)
+            line(3, " <gold><b>⛁</b> <gradient:#fde047:#eab308>Gold:</gradient>", " <white>0")
+            line(2, " <green><b>🪓</b> <gradient:#ABE2C6:#0DD97D>Dwarves:</gradient>", " <white>0")
+            line(1, " <light_purple><b>⌚</b> <gradient:#d8b4fe:#a855f7>Time:</gradient>", " <white>0")
+            line(0, "<gray>  ────────────────")
         }
+
+    private fun startCountdown() {
+        var timer = ConfigStorage.PREPARATION_COUNTDOWN
+        async(period = TimeUnit.SECONDS(1)) {
+            with(gameStatusSidebar) {
+                updateLine(2, " <white>${onlineDwarfs.get()}")
+                updateLine(1, " <white>$timer")
+                sendSideBarUpdate(onlinePlayers)
+            }
+
+            timer--
+            if (timer < 0) {
+                cancel()
+                // start next phase
+            }
+        }
+    }
+
+    override fun onEnter() {
+        for ((player, kitType) in selectedKits) {
+            if (player.isOnline) {
+                addDwarf(player, kitType)
+            }
+        }
+        onlinePlayers.addAll(Bukkit.getOnlinePlayers())
+        gameStatusSidebar.sendSidebar(onlinePlayers)
 
         Bukkit.broadcast("<gray>Preparation phase has started, dwarfs prepare for battle!!!".withPrefix().mm())
         gameMap.dwarfSpawn.world.playSound(gameMap.dwarfSpawn, Sound.ITEM_GOAT_HORN_SOUND_0, 1f, 1f)
+        startCountdown()
 
         super.onEnter()
     }
@@ -65,6 +107,7 @@ class PreparationState(private val gameMap: GameMap, private val selectedKits: M
         dwarfTeam.addPlayer(player)
         player.teleport(gameMap.dwarfSpawn)
         KitsManager.setKit(player, kitType)
+        onlineDwarfs.incrementAndGet()
     }
 
     private fun openKitSelectionMenu(player: Player) {
@@ -98,12 +141,27 @@ class PreparationState(private val gameMap: GameMap, private val selectedKits: M
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
+
+        onlinePlayers.add(player)
         player.scoreboard = customBoard
+        gameStatusSidebar.sendSidebar(listOf(player))
 
         if (!isActiveDwarf(player)) {
             refreshToLobbyState(player)
         } else if (lastActiveInRecruiting(player)) {
             addDwarf(player, selectedKits[player]!!)
+        } else {
+            onlineDwarfs.incrementAndGet()
+        }
+    }
+
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        val player = event.player
+
+        onlinePlayers.remove(player)
+        if (isActiveDwarf(player)) {
+            onlineDwarfs.decrementAndGet()
         }
     }
 
