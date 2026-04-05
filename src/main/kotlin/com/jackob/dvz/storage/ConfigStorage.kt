e92e10db61bc5cb1ba1098b2700a099e33f85f70
Jackob2004 <zjakub2004@gmail.com>
2026-04-05 17:59:33 +0200

    feat(config): add preparation countdown to config yml

--- src/main/kotlin/com/jackob/dvz/storage/ConfigStorage.kt
+++ src/main/kotlin/com/jackob/dvz/storage/ConfigStorage.kt
@@ -4,7 +4,8 @@ import com.jackob.dvz.DvZ
 
 object ConfigStorage {
     val REQUIRED_PLAYERS = DvZ.INSTANCE.config.getInt("required-players")
-    val COUNTDOWN = DvZ.INSTANCE.config.getInt("countdown")
+    val RECRUITING_COUNTDOWN = DvZ.INSTANCE.config.getInt("recruiting-countdown")
+    val PREPARATION_COUNTDOWN = DvZ.INSTANCE.config.getInt("preparation-countdown")
     val MAP_CHANGE_TIME_LIMIT = DvZ.INSTANCE.config.getInt("map-change-time-limit")
     val PLAYERS_PER_HERO = DvZ.INSTANCE.config.getInt("players-per-hero")
     val HERO_SELECT_TIME = DvZ.INSTANCE.config.getInt("hero-select-time")
