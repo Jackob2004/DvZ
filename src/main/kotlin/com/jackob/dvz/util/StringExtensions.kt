686ce181672fbb28d3b835ea1ea20cd13d984fd9
Jackob2004 <zjakub2004@gmail.com>
2026-04-11 18:54:37 +0200

    feat(util): add toSound String extension fn

--- src/main/kotlin/com/jackob/dvz/util/StringExtensions.kt
+++ src/main/kotlin/com/jackob/dvz/util/StringExtensions.kt
@@ -1,9 +1,11 @@
 package com.jackob.dvz.util
 
+import com.jackob.dvz.DvZ
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.NamespacedKey
 import org.bukkit.Registry
+import org.bukkit.Sound
 import org.bukkit.attribute.Attribute
 
 const val PREFIX = "<gold><b>DvZ: <reset>"
@@ -16,6 +18,13 @@ fun String.withPrefix(): String {
     return PREFIX + this
 }
 
-fun String.toAttribute() : Attribute {
+fun String.toAttribute(): Attribute {
     return Registry.ATTRIBUTE.get(NamespacedKey.minecraft(this.lowercase()))!!
+}
+
+fun String.toSound(): Sound? {
+    val key = NamespacedKey.minecraft(this.lowercase())
+    val sound = Registry.SOUND_EVENT.get(key)
+    if (sound == null) DvZ.INSTANCE.logger.warning("Sound not found for key: '$key'")
+    return sound
 }
