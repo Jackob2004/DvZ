ee05368ec6742d125684c7171702c5f96507ac62
Jackob2004 <zjakub2004@gmail.com>
2026-04-19 13:11:16 +0200

    feat(custom-enchantment): add handling of first and second level in RadianceEnchantment
    Second level protects the placed source of light for 60 seconds, so it
    cannot be destroyed by hand.
    
    Refs: #31

--- src/main/kotlin/com/jackob/dvz/core/enchantments/CustomEnchantmentRegistry.kt
+++ src/main/kotlin/com/jackob/dvz/core/enchantments/CustomEnchantmentRegistry.kt
@@ -1,9 +1,11 @@
 package com.jackob.dvz.core.enchantments
 
+import com.jackob.dvz.DvZ
 import io.papermc.paper.registry.RegistryAccess
 import io.papermc.paper.registry.RegistryKey
 import net.kyori.adventure.key.Key
 import org.bukkit.enchantments.Enchantment
+import org.bukkit.event.Listener
 
 object CustomEnchantmentRegistry {
 
@@ -18,6 +20,12 @@ object CustomEnchantmentRegistry {
     lateinit var RADIANCE: Enchantment
 
     fun init() {
+        for (e in enchantments) {
+            if (e is Listener) {
+                DvZ.INSTANCE.server.pluginManager.registerEvents(e, DvZ.INSTANCE)
+            }
+        }
+
         val registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT)
         RADIANCE = registry.get(Key.key("dvz:radiance"))!!
     }
--- src/main/kotlin/com/jackob/dvz/core/enchantments/RadianceEnchantment.kt
+++ src/main/kotlin/com/jackob/dvz/core/enchantments/RadianceEnchantment.kt
@@ -1,5 +1,7 @@
 package com.jackob.dvz.core.enchantments
 
+import com.jackob.dvz.util.TimeUnit
+import com.jackob.dvz.util.mm
 import io.papermc.paper.registry.RegistryKey
 import io.papermc.paper.registry.TypedKey
 import io.papermc.paper.registry.data.EnchantmentRegistryEntry
@@ -8,13 +10,24 @@ import io.papermc.paper.registry.keys.ItemTypeKeys
 import io.papermc.paper.registry.set.RegistrySet
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.text.Component
+import org.bukkit.Location
+import org.bukkit.entity.TextDisplay
 import org.bukkit.enchantments.Enchantment
-import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.inventory.EquipmentSlotGroup
+import com.jackob.dvz.util.sync
+import org.bukkit.Sound
+import org.bukkit.entity.Display
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
 
 @Suppress("UnstableApiUsage")
-class RadianceEnchantment : CustomEnchantment(), InteractionEnchantment {
+class RadianceEnchantment : CustomEnchantment(), PlacementEnchantment, Listener{
+
+    private val protectedBlocks: MutableSet<Location> = HashSet()
 
     override val key: TypedKey<Enchantment> = EnchantmentKeys.create(Key.key("dvz:radiance"))
 
@@ -24,7 +37,7 @@ class RadianceEnchantment : CustomEnchantment(), InteractionEnchantment {
                 RegistrySet.keySet(
                     RegistryKey.ITEM,
                     ItemTypeKeys.TORCH,
-                    ItemTypeKeys.LANTERN,
+                    ItemTypeKeys.SHROOMLIGHT,
                     ItemTypeKeys.SOUL_TORCH,
                 )
             )
@@ -33,9 +46,48 @@ class RadianceEnchantment : CustomEnchantment(), InteractionEnchantment {
         applyCommonConfig(b)
     }
 
-    override fun handleItemUse(event: PlayerInteractEvent, level: Int) {
-        if (event.action != Action.RIGHT_CLICK_BLOCK) return
-        event.player.sendMessage("$level")
+    fun handleSecondLevel(placedBlockLocation : Location, player: Player) {
+        protectedBlocks.add(placedBlockLocation)
+
+        var timeLeft = 60
+        val displayLoc = placedBlockLocation.clone().add(0.5, 1.5, 0.5)
+        val timeDisplay = (placedBlockLocation.world.spawnEntity(displayLoc, EntityType.TEXT_DISPLAY) as TextDisplay).apply {
+            text("<gray>$timeLeft".mm())
+            billboard = Display.Billboard.CENTER
+        }
+
+        sync(period = TimeUnit.SECONDS(1)) {
+            timeLeft--
+            if (timeLeft <= 0) {
+                protectedBlocks.remove(placedBlockLocation)
+                timeDisplay.remove()
+                this.cancel()
+            } else {
+                timeDisplay.text("<gray>$timeLeft".mm())
+            }
+        }
+
+        player.playSound(displayLoc, Sound.BLOCK_CAMPFIRE_CRACKLE, 1f, 1f)
     }
 
+    override fun handleBlockPlace(event: BlockPlaceEvent, level: Int) {
+        val placedBlock = event.blockPlaced
+        val placedBlockLocation = placedBlock.location
+        val player = event.player
+
+        placedBlock.drops.clear()
+
+        when (level) {
+            3 -> TODO("Implement handling 3th level")
+            2 -> handleSecondLevel(placedBlockLocation, player)
+            1 -> player.playSound(placedBlockLocation, Sound.BLOCK_FIRE_AMBIENT, 1f, 1f)
+        }
+    }
+
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) {
+        if (protectedBlocks.contains(event.block.location)) {
+            event.isCancelled = true
+        }
+    }
 }
