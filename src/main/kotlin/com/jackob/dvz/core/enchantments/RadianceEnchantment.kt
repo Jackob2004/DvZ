42ca539a395780589e17054186c4714fa070bf27
Jackob2004 <zjakub2004@gmail.com>
2026-04-21 15:52:47 +0200

    feat(custom-enchantment): handle third level of radiance enchantment
    Add light blast effect.
    Refactor method handling second level: change name, add duration in seconds param.
    
    Refs: #31

--- src/main/kotlin/com/jackob/dvz/core/enchantments/RadianceEnchantment.kt
+++ src/main/kotlin/com/jackob/dvz/core/enchantments/RadianceEnchantment.kt
@@ -19,13 +19,17 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.inventory.EquipmentSlotGroup
 import com.jackob.dvz.util.sync
+import org.bukkit.Material
+import org.bukkit.Particle
 import org.bukkit.Sound
+import org.bukkit.block.Block
 import org.bukkit.entity.Display
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
+import kotlin.math.exp
 
 @Suppress("UnstableApiUsage")
-class RadianceEnchantment : CustomEnchantment(), PlacementEnchantment, Listener{
+class RadianceEnchantment : CustomEnchantment(), PlacementEnchantment, Listener {
 
     private val protectedBlocks: MutableSet<Location> = HashSet()
 
@@ -46,15 +50,16 @@ class RadianceEnchantment : CustomEnchantment(), PlacementEnchantment, Listener{
         applyCommonConfig(b)
     }
 
-    fun handleSecondLevel(placedBlockLocation : Location, player: Player) {
+    fun spawnProtectedLightBlock(placedBlockLocation: Location, player: Player, durationInSeconds: Int) {
         protectedBlocks.add(placedBlockLocation)
 
-        var timeLeft = 60
+        var timeLeft = durationInSeconds
         val displayLoc = placedBlockLocation.clone().add(0.5, 1.5, 0.5)
-        val timeDisplay = (placedBlockLocation.world.spawnEntity(displayLoc, EntityType.TEXT_DISPLAY) as TextDisplay).apply {
-            text("<gray>$timeLeft".mm())
-            billboard = Display.Billboard.CENTER
-        }
+        val timeDisplay =
+            (placedBlockLocation.world.spawnEntity(displayLoc, EntityType.TEXT_DISPLAY) as TextDisplay).apply {
+                text("<gray>$timeLeft".mm())
+                billboard = Display.Billboard.CENTER
+            }
 
         sync(period = TimeUnit.SECONDS(1)) {
             timeLeft--
@@ -70,6 +75,42 @@ class RadianceEnchantment : CustomEnchantment(), PlacementEnchantment, Listener{
         player.playSound(displayLoc, Sound.BLOCK_CAMPFIRE_CRACKLE, 1f, 1f)
     }
 
+    fun playLightBlastEffect(placedBlock: Block, player: Player, durationInSeconds: Int) {
+        placedBlock.type = Material.LIGHT
+
+        val durationInTicks = durationInSeconds * 10
+        var timeLeft = durationInTicks
+
+        val effectLoc = placedBlock.location
+        val particleEffect = Particle.END_ROD.builder()
+            .location(effectLoc.add(0.0, 2.0, 0.0))
+            .count(1)
+            .receivers(8, true)
+            .offset(0.0, 0.0, 0.0)
+            .extra(0.1)
+
+        val initialStrength = 30
+        val decayRate = 5.0
+        var elapsed = 0.0
+
+        sync(period = TimeUnit.TICKS(2)) {
+            timeLeft--
+            elapsed++
+            if (timeLeft <= 0) {
+                placedBlock.type = Material.AIR
+                this.cancel()
+            } else {
+                val time = elapsed / durationInTicks
+                // exponential decay
+                val effectIntensity = (initialStrength * exp(-decayRate * time)).toInt()
+                particleEffect.count(effectIntensity)
+                particleEffect.spawn()
+            }
+        }
+
+        player.playSound(effectLoc, Sound.BLOCK_LAVA_EXTINGUISH, 1f, 1f)
+    }
+
     override fun handleBlockPlace(event: BlockPlaceEvent, level: Int) {
         val placedBlock = event.blockPlaced
         val placedBlockLocation = placedBlock.location
@@ -78,8 +119,8 @@ class RadianceEnchantment : CustomEnchantment(), PlacementEnchantment, Listener{
         placedBlock.drops.clear()
 
         when (level) {
-            3 -> TODO("Implement handling 3th level")
-            2 -> handleSecondLevel(placedBlockLocation, player)
+            3 -> playLightBlastEffect(placedBlock, player, 15)
+            2 -> spawnProtectedLightBlock(placedBlockLocation, player, 60)
             1 -> player.playSound(placedBlockLocation, Sound.BLOCK_FIRE_AMBIENT, 1f, 1f)
         }
     }
