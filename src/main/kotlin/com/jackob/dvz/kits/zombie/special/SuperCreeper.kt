2f836542f07c3154bd5cbdaca2543bc44745f07a
Jackob2004 <zjakub2004@gmail.com>
2026-07-01 09:55:51 +0200

    feat(kits): add super creeper kit skeleton

---
+++ src/main/kotlin/com/jackob/dvz/kits/zombie/special/SuperCreeper.kt
@@ -0,0 +1,42 @@
+package com.jackob.dvz.kits.zombie.special
+
+import com.jackob.dvz.DvZ
+import com.jackob.dvz.kits.BaseKit
+import com.jackob.dvz.kits.Disguisable
+import com.jackob.dvz.util.toPlayer
+import me.libraryaddict.disguise.disguisetypes.Disguise
+import me.libraryaddict.disguise.disguisetypes.DisguiseType
+import me.libraryaddict.disguise.disguisetypes.watchers.CreeperWatcher
+import org.bukkit.event.Listener
+import java.util.UUID
+
+class SuperCreeper(internalName: String, owner: UUID) : BaseKit(internalName, owner), Disguisable<CreeperWatcher> {
+
+    override val disguiseTemplate: Disguise = createMobDisguise(DisguiseType.CREEPER) {
+        isIgnited = true
+    }
+
+    override val isHero: Boolean = true
+
+    override val aiZombieEnabled: Boolean = true
+
+    override fun onActivate() {
+        super.onActivate()
+        startDisguise(ownerId.toPlayer()!!)
+    }
+
+    override fun onDeactivate() {
+        super.onDeactivate()
+        stopDisguise(ownerId.toPlayer()!!)
+    }
+
+    init {
+        SuperCreeperListener
+    }
+
+    object SuperCreeperListener: Listener {
+        init {
+            DvZ.INSTANCE.server.pluginManager.registerEvents(this, DvZ.INSTANCE)
+        }
+    }
+}
