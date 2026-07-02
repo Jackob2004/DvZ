54139a0fc96b69121afdfdfe151695c9c7178dd5
Jackob2004 <zjakub2004@gmail.com>
2026-07-02 16:45:17 +0200

    feat(kits): add first tier of zombie upgrdes

--- src/main/kotlin/com/jackob/dvz/kits/zombie/base/Zombie.kt
+++ src/main/kotlin/com/jackob/dvz/kits/zombie/base/Zombie.kt
@@ -1,6 +1,7 @@
 package com.jackob.dvz.kits.zombie.base
 
 import com.jackob.dvz.DvZ
+import com.jackob.dvz.core.events.AIZombieSpawnEvent
 import com.jackob.dvz.kits.BaseKit
 import com.jackob.dvz.kits.BasePath
 import com.jackob.dvz.kits.Disguisable
@@ -16,14 +17,17 @@ import me.libraryaddict.disguise.disguisetypes.DisguiseType
 import me.libraryaddict.disguise.disguisetypes.watchers.LivingWatcher
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import java.util.UUID
+import kotlin.random.Random
 
-class Zombie(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(internalName, owner, isHero), Disguisable<LivingWatcher> {
+class Zombie(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(internalName, owner, isHero),
+    Disguisable<LivingWatcher> {
 
     override val disguiseTemplate: Disguise = createMobDisguise(DisguiseType.ZOMBIE) { }
 
@@ -49,181 +53,120 @@ class Zombie(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(inter
 
     object ZombieListener : Listener {
 
-        val upgrades = UpgradesManager.create(ZombieUpgrade.entries.size, 2, ZombiePath.entries.size) {
+        val upgrades = UpgradesManager.create(ZombieUpgrade.entries.size, 1, ZombiePath.entries.size) {
             tier(0) {
-
-                upgrade(ZombieUpgrade.UNDEAD_I, UpgradeType.MODIFIER, 6, ZombiePath.UNDEAD) {
-                    icon = createItem(Material.REDSTONE) {
-                        name = "<blue>Undead"
+                upgrade(ZombieUpgrade.INFECTION_I, UpgradeType.PASSIVE_ABILITY, 1, ZombiePath.UNDEAD) {
+                    icon = createItem(Material.POISONOUS_POTATO) {
+                        name = "<dark_purple>Infection"
                         description = """
-                           <gray>Gives you a chance to be reborn in the placed you last died
-                        """
-                    }
-
-                    level(1)
-                    level(2)
-
-                    action { player, modifier ->
-                        player.inventory.addItem(createItem(Material.RED_STAINED_GLASS) {
-                            name = "<red>Undead $modifier"
-                        })
-                    }
-                }
-
-                upgrade(ZombieUpgrade.GIANT_I,UpgradeType.MODIFIER, 7, ZombiePath.GIANT) {
-                    icon = createItem(Material.YELLOW_STAINED_GLASS) {
-                        name = "<blue>Giant"
-                        description = """
-                           <gray>Makes you bigger, slower and stronger zombie
-                        """
-                    }
-
-                    level(1)
-                    level(2)
-
-                    action { player, modifier ->
-                        player.inventory.addItem(createItem(Material.YELLOW_STAINED_GLASS) {
-                            name = "<yellow>Giant $modifier"
-                        })
-                    }
-                }
-
-                upgrade(ZombieUpgrade.CAPTAIN_I, UpgradeType.MODIFIER, 4, ZombiePath.CAPTAIN) {
-                    icon = createItem(Material.BLACK_BANNER) {
-                        name = "<blue>Captain"
-                        description = """
-                           <gray>Gives you ability to buff other zombies
-                        """
-                    }
-
-                    level(1)
-
-                    action { player, modifier ->
-                        player.inventory.addItem(createItem(Material.BLACK_BANNER) {
-                            name = "<red>Captain$modifier"
-                        })
-                    }
-                }
-                upgrade(ZombieUpgrade.TOUGH_FLESH_I, UpgradeType.MODIFIER, 2) {
-                    icon = createItem(Material.ROTTEN_FLESH) {
-                        name = "<dark_green>Tough Flesh"
-                        description = """
-                           <gray>Increases number of hearts by one on each level 
+                           <gray>Gives you 2% chance of giving dwarf poison effect I on hitting him.
+                           <gray>Effect lasts 5 seconds.
+                           <gray>Chance increases each level by 2%.
                         """
                     }
 
                     (2..6 step 2).forEach { level(it) }
 
-                    action { player, modifier ->
-                        val maxHealthAttr = player.getAttribute(Attribute.MAX_HEALTH) ?: return@action
-                        maxHealthAttr.baseValue += modifier
-                        player.health = maxHealthAttr.value
+                    action { dwarfVictim, chance ->
+                        if (Random.nextInt(100) >= chance) return@action
+                        dwarfVictim.addPotionEffect(PotionEffect(PotionEffectType.POISON, 5 * 20, 0))
+                    }
+                }
+
+                upgrade(ZombieUpgrade.HARDENED_FLESH_I, UpgradeType.MODIFIER, 1, ZombiePath.GIANT) {
+                    icon = createItem(Material.COPPER_CHESTPLATE) {
+                        name = "<green>Hardened flesh"
+                        description = """
+                           <gray>Gives you 2 golden hearts.
+                           <gray>Increases by 2 each level.
+                        """
+                    }
+
+                    (0..2).forEach { level(it) }
+
+                    action { zombiePlayer, modifier ->
+                        zombiePlayer.addPotionEffect(PotionEffect(PotionEffectType.ABSORPTION, Int.MAX_VALUE, modifier))
+                    }
+                }
+
+                upgrade(ZombieUpgrade.LEADERSHIP_I, UpgradeType.PASSIVE_ABILITY, 1, ZombiePath.CAPTAIN) {
+                    icon = createItem(Material.GOLDEN_HELMET) {
+                        name = "<gold>Leadership"
+                        description = """
+                           <gray>Half of your AI zombies spawned around you will
+                           <gray>receive buff of 2 golden hearts.
+                           <gray>Increases by 2 each level.
+                        """
+                    }
+
+                    (0..2).forEach { level(it) }
+
+                    action { zombieEntity, modifier ->
+                        zombieEntity.addPotionEffect(PotionEffect(PotionEffectType.ABSORPTION, Int.MAX_VALUE, modifier))
+                    }
+                }
+
+                upgrade(ZombieUpgrade.RAW_DAMAGE_I, UpgradeType.MODIFIER, 1) {
+                    icon = createItem(Material.IRON_SWORD) {
+                        name = "<gold>Raw damage"
+                        description = """
+                           <gray>Increases base attack damage by 1 heart each level
+                        """
+                    }
+
+                    (2..6 step 2).forEach { level(it) }
+
+                    action { zombiePlayer , modifier ->
+                        val attr = zombiePlayer.getAttribute(Attribute.ATTACK_DAMAGE) ?: return@action
+                        attr.baseValue += modifier
                     }
                 }
 
             }
 
-            tier(1) {
-                upgrade(ZombieUpgrade.RAW_DAMAGE_I, UpgradeType.MODIFIER, 3, ZombiePath.UNDEAD) {
-                    icon = createItem(Material.COPPER_SWORD) {
-                        name = "<dark_red>Raw Damage"
-                        description = """
-                           <gray>Increases zombie's base damage
-                        """
-                    }
-
-                    level(1.0)
-                    level(3.0)
-
-                    action { player, modifier ->
-                        val maxAttackAttr = player.getAttribute(Attribute.ATTACK_DAMAGE) ?: return@action
-                        maxAttackAttr.baseValue += modifier
-                    }
-                }
-
-                upgrade(ZombieUpgrade.LEAP_I, UpgradeType.ACTIVE_ABILITY, 5, ZombiePath.GIANT) {
-                    icon = createItem(Material.RABBIT_HIDE) {
-                        name = "<blue>Leap"
-                        description = """
-                           <gray>Gives a zombie ability to leap by right clicking on their blade
-                        """
-                    }
-
-                    level(1.5)
-                    level(2.2)
-
-                    action { player, modifier ->
-                        val vector = player.eyeLocation.direction.normalize().multiply(modifier)
-                        player.velocity = vector
-                    }
-                }
-
-                upgrade(ZombieUpgrade.VAMPIRE, UpgradeType.MODIFIER, 1, ZombiePath.CAPTAIN) {
-                    icon = createItem(Material.QUARTZ_BLOCK) {
-                        name = "<blue>Vampire"
-                        description = """
-                           <gray>Test
-                        """
-                    }
-
-                    level(1)
-
-                    action { player, modifier ->
-                        player.sendMessage("Test $modifier")
-                    }
-                }
-
-                upgrade(ZombieUpgrade.INFECTION, UpgradeType.PASSIVE_ABILITY, 1) {
-                    icon = createItem(Material.POISONOUS_POTATO) {
-                        name = "<blue>Infection"
-                        description = """
-                           <gray>Test2
-                        """
-                    }
-
-                    level(1)
-
-                    action { player, modifier ->
-                        player.sendMessage("infection$modifier")
-                    }
-                }
-            }
         }
 
         init {
             DvZ.INSTANCE.server.pluginManager.registerEvents(this, DvZ.INSTANCE)
         }
 
+        @EventHandler
+        fun onZombieDealDamage(e: EntityDamageByEntityEvent) {
+            val attacker = e.damager as? Player ?: return
+            val upgrade = ZombieUpgrade.INFECTION_I
+            if (!upgrades.hasUpgrade(attacker, upgrade)) return
+            if (KitsManager.getKit(attacker) !is Zombie) return
+
+            val dwarfVictim = e.entity as? Player ?: return
+            upgrades.applyAbility(dwarfVictim, upgrade, 0)
+        }
 
         @EventHandler
-        fun onBladeClick(e: PlayerInteractEvent) {
-            val player = e.player
-            val upgrade = ZombieUpgrade.LEAP_I
-
+        fun onAIZombieSpawn(e: AIZombieSpawnEvent) {
+            val player = e.zombiePlayer
+            val upgrade = ZombieUpgrade.LEADERSHIP_I
             if (!upgrades.hasUpgrade(player, upgrade)) return
             if (KitsManager.getKit(player) !is Zombie) return
-            if (e.action != Action.RIGHT_CLICK_AIR) return
-            if (e.hand != EquipmentSlot.HAND) return
-            if (player.inventory.itemInMainHand.type != Material.WOODEN_SWORD) return
 
-            upgrades.applyAbility(player, upgrade, 0)
+            for (i in 0..(e.zombies.size / 2).minus(1)) {
+                upgrades.applyAbility(e.zombies[i], upgrade, 0)
+            }
         }
 
         enum class ZombieUpgrade {
-            UNDEAD_I,
-            UNDEAD_II,
-            GIANT_I,
-            GIANT_II,
-            CAPTAIN_I,
-            TOUGH_FLESH_I,
-            TOUGH_FLESH_II,
-            TOUGH_FLESH_III,
+            INFECTION_I,
+            INFECTION_II,
+            INFECTION_III,
+            HARDENED_FLESH_I,
+            HARDENED_FLESH_II,
+            HARDENED_FLESH_III,
+            LEADERSHIP_I,
+            LEADERSHIP_II,
+            LEADERSHIP_III,
             RAW_DAMAGE_I,
             RAW_DAMAGE_II,
-            LEAP_I,
-            LEAP_II,
-            VAMPIRE,
-            INFECTION
+            RAW_DAMAGE_III,
+            RAW_DAMAGE_IV,
         }
 
         enum class ZombiePath(override val pathName: String) : BasePath {
