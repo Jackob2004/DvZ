c88896a7e634291292cb05acc171fe2b2a519c8b
Jackob2004 <zjakub2004@gmail.com>
2026-07-04 09:11:59 +0200

    feat(utils): add more functions to LocationExtension
    Add functions for packing and unpacking Location object
    by storing it in long using bitwise operations.

--- src/main/kotlin/com/jackob/dvz/util/LocationExtension.kt
+++ src/main/kotlin/com/jackob/dvz/util/LocationExtension.kt
@@ -1,6 +1,7 @@
 package com.jackob.dvz.util
 
 import org.bukkit.Location
+import org.bukkit.World
 import org.bukkit.block.Block
 
 fun Location.getSphere(radius: Int, hollow: Boolean): MutableList<Location> {
@@ -34,4 +35,27 @@ fun Block.packCoordinates(): Long {
     return ((x and 0x3FFFFFF).toLong() shl 38) or
             ((z and 0x3FFFFFF).toLong() shl 12) or
             ((y and 0xFFF).toLong())
+}
+
+/**
+ * Packs X, Y, and Z into a single 64-bit long.
+ * X: 26 bits | Z: 26 bits | Y: 12 bits
+ */
+fun Location.packCoordinates(): Long {
+    return ((x.toLong() and 0x3FFFFFF) shl 38) or
+            ((z.toLong() and 0x3FFFFFF) shl 12) or
+            ((y.toLong() and 0xFFF))
+}
+
+/**
+ * Expects bits to be packed in the following manner:
+ * X: 26 bits | Z: 26 bits | Y: 12 bits
+ */
+fun Long.unpackToLocation(world: World): Location {
+    val x = (this shr 38).toInt()
+
+    val z = (((this shr 12).toInt() and 0x3FFFFFF) shl 6) shr 6
+    val y = ((this and 0xFFF).toInt() shl 20) shr 20
+
+    return Location(world, x.toDouble(), y.toDouble(), z.toDouble())
 }
