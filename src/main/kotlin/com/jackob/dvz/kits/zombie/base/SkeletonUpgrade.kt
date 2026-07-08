11d96777d07a6f73fc71f77a0cc6c3a738cc2e55
Jackob2004 <zjakub2004@gmail.com>
2026-07-08 18:51:29 +0200

    feat(kits): add SkeletonUpgrade enum
    Define first tier upgrades.

---
+++ src/main/kotlin/com/jackob/dvz/kits/zombie/base/SkeletonUpgrade.kt
@@ -0,0 +1,14 @@
+package com.jackob.dvz.kits.zombie.base
+
+enum class SkeletonUpgrade {
+    CLOAK_I,
+    CLOAK_II,
+    CLOAK_III,
+    ARMORY_I,
+    ARMORY_II,
+    ARMORY_III,
+    LEATHER_QUIVER_I,
+    LEATHER_QUIVER_II,
+    LEATHER_QUIVER_III,
+    LEATHER_QUIVER_IV,
+}
