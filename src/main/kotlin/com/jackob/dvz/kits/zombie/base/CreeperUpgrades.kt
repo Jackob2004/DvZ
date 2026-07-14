70f08432d3d2040503de1c79baad80a1d38e4918
Jackob2004 <zjakub2004@gmail.com>
2026-07-14 17:01:27 +0200

    feat(kits): define tier 1 creeper upgrades
    Refs: #49

---
+++ src/main/kotlin/com/jackob/dvz/kits/zombie/base/CreeperUpgrades.kt
@@ -0,0 +1,11 @@
+package com.jackob.dvz.kits.zombie.base
+
+enum class CreeperUpgrades {
+    SHALLOW_WOUND_I,
+    SHALLOW_WOUND_II,
+    SHALLOW_WOUND_III,
+    WALL_CRUSHER_I,
+    WALL_CRUSHER_II,
+    WALL_CRUSHER_III,
+    HARDENING_I
+}
