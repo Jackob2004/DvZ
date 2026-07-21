2958c8cb2305fca5e8ba45e5c3ebc00b84e39f78
Jackob2004 <zjakub2004@gmail.com>
2026-07-21 17:22:40 +0200

    feat(kits): extend Disguisable interface functionality
    Add createPlayerDisguise fn.

--- src/main/kotlin/com/jackob/dvz/kits/Disguisable.kt
+++ src/main/kotlin/com/jackob/dvz/kits/Disguisable.kt
@@ -4,6 +4,7 @@ import me.libraryaddict.disguise.DisguiseAPI
 import me.libraryaddict.disguise.disguisetypes.Disguise
 import me.libraryaddict.disguise.disguisetypes.DisguiseType
 import me.libraryaddict.disguise.disguisetypes.MobDisguise
+import me.libraryaddict.disguise.disguisetypes.PlayerDisguise
 import me.libraryaddict.disguise.disguisetypes.watchers.LivingWatcher
 import org.bukkit.entity.Player
 
@@ -24,13 +25,30 @@ interface Disguisable<T: LivingWatcher> {
     }
 
     @Suppress("UNCHECKED_CAST")
+    fun createPlayerDisguise(skinName: String, nickname: String = " ", config: T.() -> Unit): Disguise {
+        val disguise = PlayerDisguise(nickname)
+        disguise.setViewSelfDisguise(false)
+
+        disguise.skin = skinName
+
+        val watcher = disguise.watcher as? T
+        watcher?.config()
+
+        return disguise
+    }
+
+    @Suppress("UNCHECKED_CAST")
     fun Player.modifyMobDisguise(config: T.() -> Unit) {
         val watcher = DisguiseAPI.getDisguise(this)?.watcher as? T ?: return
         watcher.config()
     }
 
     fun startDisguise(player: Player) {
-        val cloned = disguiseTemplate.clone()
+        startDisguise(player, disguiseTemplate)
+    }
+
+    fun startDisguise(player: Player, template: Disguise) {
+        val cloned = template.clone()
         cloned.entity = player
         cloned.startDisguise()
     }
