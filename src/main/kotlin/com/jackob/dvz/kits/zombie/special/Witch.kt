3e52216119e198f9a6eb9f70ed9c68778d387bc9
Jackob2004 <zjakub2004@gmail.com>
2026-07-27 17:02:58 +0200

    feat(kits): add potion brewing ability to witch kit

--- src/main/kotlin/com/jackob/dvz/kits/zombie/special/Witch.kt
+++ src/main/kotlin/com/jackob/dvz/kits/zombie/special/Witch.kt
@@ -1,40 +1,152 @@
 package com.jackob.dvz.kits.zombie.special
 
 import com.jackob.dvz.DvZ
+import com.jackob.dvz.core.handlers.GameplayMechanicsHandler.Companion.UNPLACEABLE_KEY
 import com.jackob.dvz.kits.BaseKit
 import com.jackob.dvz.kits.Disguisable
+import com.jackob.dvz.kits.KitsManager
+import com.jackob.dvz.util.CooldownUtil
+import com.jackob.dvz.util.createItem
+import com.jackob.dvz.util.description
+import com.jackob.dvz.util.mm
+import com.jackob.dvz.util.name
+import com.jackob.dvz.util.rightClickItem
 import com.jackob.dvz.util.toPlayer
+import com.jackob.dvz.util.withCooldown
 import me.libraryaddict.disguise.disguisetypes.Disguise
 import me.libraryaddict.disguise.disguisetypes.DisguiseType
 import me.libraryaddict.disguise.disguisetypes.watchers.WitchWatcher
+import org.bukkit.Color
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.meta.PotionMeta
+import org.bukkit.persistence.PersistentDataType
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import java.util.UUID
+import kotlin.random.Random
 
-class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(internalName, owner, isHero), Disguisable<WitchWatcher> {
+class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(internalName, owner, isHero),
+    Disguisable<WitchWatcher> {
 
     override val disguiseTemplate: Disguise = createMobDisguise(DisguiseType.WITCH) {
         isAggressive = true
     }
 
-    override val aiZombieEnabled: Boolean = true
+    override val aiZombieEnabled: Boolean = false
 
-    override fun onActivate() {
-        super.onActivate()
-        startDisguise(ownerId.toPlayer()!!)
-    }
+    companion object {
+        private const val BREWING_COOLDOWN = 25
 
-    override fun onDeactivate() {
-        super.onDeactivate()
-        stopDisguise(ownerId.toPlayer()!!)
+        private val brewingCooldowns = CooldownUtil(BREWING_COOLDOWN * 1000L)
+
+        private val magicBrewingStand = createItem(Material.BREWING_STAND) {
+            name = "<light_purple>Magic Brewing Stand"
+            description = """
+                Produces three random batches of potions
+                Cooldown <gray>${BREWING_COOLDOWN}s
+                <green>[Right] <white>-click to use
+            """
+            persistentDataContainer.set(UNPLACEABLE_KEY, PersistentDataType.BOOLEAN, true)
+        }
+
+        private const val BASE_POTION_AMPLIFIER = 0
+
+        private val possibleBaseEffects = arrayOf(
+            CustomPotion(
+                PotionEffect(PotionEffectType.INSTANT_HEALTH, 1, BASE_POTION_AMPLIFIER, false, false),
+                Color.RED,
+                "<red>Health"
+            ),
+            CustomPotion(
+                PotionEffect(PotionEffectType.INSTANT_DAMAGE, 1, BASE_POTION_AMPLIFIER, false, false),
+                Color.PURPLE,
+                "<dark_purple>Damage"
+            ),
+            CustomPotion(
+                PotionEffect(PotionEffectType.POISON, 16 * 20, BASE_POTION_AMPLIFIER, false, false),
+                Color.LIME,
+                "<green>Poison"
+            ),
+            CustomPotion(
+                PotionEffect(PotionEffectType.NAUSEA, 16 * 20, BASE_POTION_AMPLIFIER, false, false),
+                Color.GREEN,
+                "<dark_green>Nausea"
+            ),
+            CustomPotion(
+                PotionEffect(PotionEffectType.WEAKNESS, 16 * 20, BASE_POTION_AMPLIFIER, false, false),
+                Color.BLACK,
+                "<gray>Weakness"
+            ),
+            CustomPotion(
+                PotionEffect(PotionEffectType.SLOWNESS, 16 * 20, BASE_POTION_AMPLIFIER, false, false),
+                Color.AQUA,
+                "<aqua>Slowness"
+            ),
+        )
     }
 
     init {
         WitchListener
     }
 
-    object WitchListener: Listener {
+    override fun onActivate() {
+        super.onActivate()
+        val player = ownerId.toPlayer()!!
+        startDisguise(player)
+
+        player.inventory.addItem(magicBrewingStand)
+        player.playSound(player.location, Sound.ENTITY_WITCH_AMBIENT, 1f, 1f)
+    }
+
+    override fun onDeactivate() {
+        super.onDeactivate()
+        val player = ownerId.toPlayer()!!
+        stopDisguise(player)
+
+        brewingCooldowns.removeFromCooldown(player)
+    }
+
+    private fun brewPotions(witchPlayer: Player) = witchPlayer.withCooldown(brewingCooldowns) {
+        val randomPicks = Array(3) {
+            Random.nextInt(possibleBaseEffects.size)
+        }
+        val amount = 16
+
+        for (idx in randomPicks) {
+            val customPotion = possibleBaseEffects[idx]
+            inventory.addItem(createItem<PotionMeta>(Material.SPLASH_POTION, amount) {
+                color = customPotion.color
+
+                addCustomEffect(customPotion.potion, true)
+                customName(customPotion.name.mm())
+                setMaxStackSize(amount)
+            })
+        }
+
+        playSound(location, Sound.BLOCK_BREWING_STAND_BREW, 1f, 1f)
+    }
+
+    object WitchListener : Listener {
         init {
             DvZ.INSTANCE.server.pluginManager.registerEvents(this, DvZ.INSTANCE)
         }
+
+        @EventHandler
+        fun onItemClick(e: PlayerInteractEvent) {
+            val player = e.player
+            val witchKit = KitsManager.getKit(player) as? Witch ?: return
+
+            val rightClickedItem = e.rightClickItem
+            if (rightClickedItem == magicBrewingStand) {
+                witchKit.brewPotions(player)
+            }
+        }
     }
+
+    data class CustomPotion(val potion: PotionEffect, val color: Color, val name: String)
 }
