943e87863dbc19b6991d44050982132dd7d6a0af
Jackob2004 <zjakub2004@gmail.com>
2026-07-30 18:58:34 +0200

    feat(kits): implement magic cauldron's main functionality
    Refs: #61

--- src/main/kotlin/com/jackob/dvz/kits/zombie/special/Witch.kt
+++ src/main/kotlin/com/jackob/dvz/kits/zombie/special/Witch.kt
@@ -14,27 +14,28 @@ import me.libraryaddict.disguise.disguisetypes.watchers.WitchWatcher
 import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.Tag
 import org.bukkit.block.Block
-import org.bukkit.entity.BlockDisplay
-import org.bukkit.entity.Display
-import org.bukkit.entity.Interaction
-import org.bukkit.entity.Player
-import org.bukkit.entity.TextDisplay
+import org.bukkit.block.data.Levelled
+import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.PotionSplashEvent
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.PotionMeta
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.random.Random
-
 
 class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(internalName, owner, isHero),
     Disguisable<WitchWatcher> {
@@ -61,7 +62,7 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
             description = """
                 Produces three random batches of potions
                 Cooldown <gray>${BREWING_COOLDOWN}s
-                <green>[Right] <white>-click to use
+                <green>[Right] <white>- click to use
             """
             persistentDataContainer.set(UNPLACEABLE_KEY, PersistentDataType.BOOLEAN, true)
         }
@@ -119,7 +120,7 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
         val player = ownerId.toPlayer()!!
         startDisguise(player)
 
-        player.inventory.addItem(magicBrewingStand, magicCauldronItem)
+        player.inventory.addItem(magicBrewingStand, magicCauldronItem, *createStartingPotions())
         player.playSound(player.location, Sound.ENTITY_WITCH_AMBIENT, 1f, 1f)
     }
 
@@ -130,6 +131,20 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
 
         brewingCooldowns.removeFromCooldown(player)
         magicCauldron.unregisterCauldron()
+    }
+
+    private fun createStartingPotions(): Array<ItemStack> {
+        val amount = 10
+        return Array(possibleBaseEffects.size) { i ->
+            val customPotion = possibleBaseEffects[i]
+            createItem<PotionMeta>(Material.SPLASH_POTION, amount) {
+                color = customPotion.color
+
+                addCustomEffect(customPotion.potion, true)
+                customName(customPotion.name.mm())
+                setMaxStackSize(amount)
+            }
+        }
     }
 
     private fun brewPotions(witchPlayer: Player) = witchPlayer.withCooldown(brewingCooldowns) {
@@ -176,7 +191,7 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
         }
     }
 
-    private class MagicCauldron(val owner: UUID): Listener {
+    private class MagicCauldron(val owner: UUID) : Listener {
 
         private var display: BlockDisplay? = null
 
@@ -188,10 +203,26 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
 
         private var lastHit: Long = 0
 
+        private var level: Int = 0
+
         companion object {
             private const val MAX_HEALTH = 10
 
-            private const val HIT_INTERVAL = 1000
+            private const val HIT_INTERVAL = 1500
+
+            private const val MAX_LEVEL = 30
+
+            private const val MIN_ENEMY_DISTANCE = 10
+
+            private const val FILLING_RATE = 5
+
+            private const val FIRST_LEVEL_CAP = 10
+
+            private var deathPotionContents = arrayOf(
+                PotionEffect(PotionEffectType.WITHER, 12 * 20, 3, false, false),
+                PotionEffect(PotionEffectType.DARKNESS, 15 * 20, 2, false, false),
+                PotionEffect(PotionEffectType.MINING_FATIGUE, 20 * 20, 1, false, false),
+            )
         }
 
         init {
@@ -202,16 +233,16 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
             removeCauldron()
 
             health = MAX_HEALTH
-            val cauldronData = Material.CAULDRON.createBlockData()
 
-            display = location.world.spawn(location, BlockDisplay::class.java) { display ->
-                display.block = cauldronData
-            }
-
-            hitbox = spawnHitbox(location.clone().add(0.5,0.0,0.5))
+            display = spawnEmptyCauldron(location)
+            hitbox = spawnHitbox(location.clone().add(0.5, 0.0, 0.5))
             healthBar = spawnHealthBar(location.clone().add(0.5, 1.5, 0.5))
         }
 
+        fun unregisterCauldron() {
+            HandlerList.unregisterAll(this)
+            removeCauldron()
+        }
 
         private fun removeCauldron() {
             if (display == null) return
@@ -225,11 +256,7 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
             healthBar!!.remove()
             healthBar = null
             health = 0
-        }
-
-        fun unregisterCauldron() {
-            HandlerList.unregisterAll(this)
-            removeCauldron()
+            level = 0
         }
 
         private fun spawnHitbox(location: Location): Interaction {
@@ -245,6 +272,23 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
             }
         }
 
+        private fun spawnEmptyCauldron(location: Location): BlockDisplay {
+            val cauldronData = Material.CAULDRON.createBlockData()
+
+            return location.world.spawn(location, BlockDisplay::class.java) { display ->
+                display.block = cauldronData
+            }
+        }
+
+        private fun spawnFilledCauldron(location: Location): BlockDisplay {
+            val cauldronData = Material.WATER_CAULDRON.createBlockData() as Levelled
+            cauldronData.level = level / 10
+
+            return location.world.spawn(location, BlockDisplay::class.java) { display ->
+                display.block = cauldronData
+            }
+        }
+
         private fun takeDamage(damager: Player) {
             if (!Tag.ITEMS_PICKAXES.isTagged(damager.inventory.itemInMainHand.type)) return
 
@@ -254,14 +298,82 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
 
             health--
             healthBar!!.text("<green>$health/$MAX_HEALTH".mm())
+            rewardDamager(damager)
 
             if (health <= 0) {
                 removeCauldron()
             }
         }
 
+        private fun drainCauldron(amount: Int) {
+            level = max(level - amount, 0)
+
+            val displayLoc = display!!.location
+
+            display!!.remove()
+            display = if (level < FIRST_LEVEL_CAP) {
+                spawnEmptyCauldron(displayLoc)
+            } else {
+                spawnFilledCauldron(displayLoc)
+            }
+        }
+
+        private fun producePotions(witchPlayer: Player) {
+            if (level < FIRST_LEVEL_CAP) return
+
+            val potions = level / FIRST_LEVEL_CAP * 2
+
+            val deathPotion = createItem<PotionMeta>(Material.SPLASH_POTION, potions) {
+                color = Color.MAROON
+
+                for (e in deathPotionContents) {
+                    addCustomEffect(e, true)
+                }
+                customName("<b><dark_red>Potion of Death".mm())
+                setMaxStackSize(potions)
+            }
+
+            witchPlayer.inventory.addItem(deathPotion)
+            witchPlayer.playSound(witchPlayer.location, Sound.ENTITY_WITCH_DRINK, 1f, 1f)
+            drainCauldron(MAX_LEVEL)
+        }
+
+        private fun rewardDamager(damager: Player) {
+            if (level == 0) return
+
+            if (level >= FIRST_LEVEL_CAP) {
+                damager.addPotionEffect(PotionEffect(PotionEffectType.INSTANT_HEALTH, 1, 0, false, false))
+            }
+            drainCauldron(FILLING_RATE)
+        }
+
+        private fun fillCauldron(victim: Player, witchPlayer: Player) {
+            if (level == MAX_LEVEL) return
+
+            val displayLoc = display!!.location
+            if (victim.location.distanceSquared(displayLoc) > MIN_ENEMY_DISTANCE * MIN_ENEMY_DISTANCE) return
+
+            level = min(level + FILLING_RATE, MAX_LEVEL)
+
+            witchPlayer.playSound(witchPlayer.location, Sound.ENTITY_WITCH_CELEBRATE, 1f, 1f)
+            Particle.TRAIL.builder()
+                .location(victim.location.add(0.0, 1.0, 0.0))
+                .offset(1.0, 1.0, 1.0)
+                .count(8)
+                .data(Particle.Trail(displayLoc.clone().add(0.0, 0.5, 0.0), Color.MAROON, 35))
+                .receivers(16, true)
+                .spawn()
+
+            if (level < FIRST_LEVEL_CAP) return
+
+            display!!.remove()
+            display = spawnFilledCauldron(displayLoc)
+        }
+
         @EventHandler
-        fun onInteractionHit(e: EntityDamageByEntityEvent) {
+        fun onHitboxHit(e: EntityDamageByEntityEvent) {
+            if (display == null) return
+
             if (e.entity != hitbox) return
             val damager = e.damager as? Player ?: return
             if (GameManager.getPlayerTeam(damager) != TeamType.DWARF) return
@@ -270,11 +382,33 @@ class Witch(internalName: String, owner: UUID, isHero: Boolean) : BaseKit(intern
         }
 
         @EventHandler
-        fun onInteractionRightClick(event: PlayerInteractEntityEvent) {
-            // todo
+        fun onPotionSplash(e: PotionSplashEvent) {
+            if (display == null) return
+
+            if (!e.potion.effects.any { it.type == PotionEffectType.INSTANT_DAMAGE }) return
+
+            val shooter = e.potion.shooter as? Player ?: return
+            if (shooter.uniqueId != owner) return
+
+            for (enemy in e.affectedEntities) {
+                if (e.getIntensity(enemy) == 0.0) continue
+                val victim = enemy as? Player ?: continue
+
+                fillCauldron(victim, shooter)
+            }
+        }
+
+        @EventHandler
+        fun onInteractionRightClick(e: PlayerInteractEntityEvent) {
+            if (display == null) return
+            if (e.rightClicked != hitbox) return
+
+            val witchPlayer = e.player
+            if (witchPlayer.uniqueId != owner) return
+
+            producePotions(witchPlayer)
         }
     }
-
 
     data class CustomPotion(val potion: PotionEffect, val color: Color, val name: String)
 }
