112bfdb1468cbb789f2e326fa208f382afac21c1
Jackob2004 <zjakub2004@gmail.com>
2026-08-13 11:02:26 +0200

    feat(utils): add ManaUtil class

---
+++ src/main/kotlin/com/jackob/dvz/util/ManaUtil.kt
@@ -0,0 +1,79 @@
+package com.jackob.dvz.util
+
+import com.jackob.dvz.DvZ
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerItemHeldEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.scheduler.BukkitTask
+import java.util.UUID
+import kotlin.math.min
+
+class ManaUtil(val ownerId: UUID, val manaRegenRate: Int, val boundItem: ItemStack): Listener {
+
+    private var currMana = MAX_MANA
+
+    private val regenTask: BukkitTask = sync(period = TimeUnit.SECONDS(1)) {
+        val player = ownerId.toPlayer()
+        if (player != null) {
+            regenMana()
+        }
+
+        if (player != null && player.inventory.itemInMainHand == boundItem) {
+            updateUI(player)
+        }
+    }
+
+    companion object {
+        private const val MAX_MANA = 1000
+
+        val MANA_ITEM = NamespacedKey(DvZ.INSTANCE, "mana-item")
+    }
+
+    init {
+        if (!boundItem.persistentDataContainer.has(MANA_ITEM)) {
+            throw IllegalArgumentException("boundItem must have $MANA_ITEM namespaced key on it")
+        }
+        DvZ.INSTANCE.server.pluginManager.registerEvents(this, DvZ.INSTANCE)
+    }
+
+    fun unregisterManaBank() {
+        regenTask.cancel()
+        HandlerList.unregisterAll(this)
+    }
+
+    fun consumeMana(amount: Int, player: Player): Boolean {
+        if (currMana - amount < 0) return false
+
+        currMana -= amount
+        updateUI(player)
+
+        return true
+    }
+
+    private fun regenMana() {
+        currMana = min(currMana + manaRegenRate, MAX_MANA)
+    }
+
+    private fun updateUI(player: Player) {
+        val mana = (currMana * 100 / MAX_MANA / 100.0).toFloat()
+        player.exp = mana
+    }
+
+    @EventHandler
+    fun onItemSwitch(e: PlayerItemHeldEvent) {
+        val player = e.player
+        if (player.uniqueId != ownerId) return
+        val item = player.inventory.getItem(e.newSlot) ?: return
+
+        if (item == boundItem) {
+            updateUI(player)
+        } else if (!item.persistentDataContainer.has(MANA_ITEM)) {
+            player.exp = 0F
+        }
+    }
+
+}
